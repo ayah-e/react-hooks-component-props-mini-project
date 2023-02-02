96eb9f0ba43de859858479d494e04d46f0b9600c
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-01 21:38:21 -0500

    added if else to add emojis for the number of minutes the blog takes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 function Article(props){
+    let minutes = props.minutes;
+    let emoji = '☕️';
+    let emojiCount = Math.ceil(minutes/5)*5;
+
+    if (minutes >= 30) {
+        emoji = '🍱';
+        emojiCount = Math.ceil(minutes/10)*10;
+    }
+
+    const emojis = emoji.repeat(emojiCount/5);
+
     return (
         <article>
             <h3>{props.title}</h3>
-            <small>{props.date || "January 1, 1970"}</small>
+            <small>{props.date ||"January 1, 1970"}</small>
+            <small> {emojis} {minutes} min read</small>
             <p>{props.preview}</p>
-
         </article>
     )
 }
